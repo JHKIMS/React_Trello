ecf9f7315b33d848d4bb10aa258b62a233cf10e4
JHKIMS <wjdgns5131@gmail.com>
2023-01-27 08:04:23 +0900

    Trello: DranAndDrop라이브러리 설치+세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import { useRecoilState, useRecoilValue } from "recoil";
+import {DragDropContext, Draggable, Droppable} from "react-beautiful-dnd";
 
 function App(){
-  return;
+  const onDragEnd = () =>{};
+  return <DragDropContext onDragEnd={onDragEnd}>
+    <div>
+      <Droppable droppableId="one">
+        {() => (
+          <ul>
+            <Draggable draggableId="first" index={0}>
+              {() => <li>One</li>}
+            </Draggable>
+            <Draggable draggableId="second" index={1}>
+              {() => <li>Two</li>}
+            </Draggable>
+          </ul>
+        )}
+      </Droppable>
+    </div>
+  </DragDropContext>;
 }
 
 export default App;
