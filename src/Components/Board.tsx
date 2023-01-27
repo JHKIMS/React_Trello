602caf76a784dfe7560764e9b46d06e02ffa9299
JHKIMS <wjdgns5131@gmail.com>
2023-01-27 14:34:01 +0900

    Trello: 드래그할때 보드 색상 변경(snapshot)

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -3,11 +3,14 @@ import styled from "styled-components";
 import DragabbleCard from "./DragabbleCard";
 
 const BoardWrapper = styled.div`
+  width: 300px;
   padding: 20px 10px;
   padding-top: 30px;
   background-color: ${(props) => props.theme.boardColor};
   border-radius: 5px;
-  min-height: 200px;
+  min-height: 300px;
+  display: flex;
+  flex-direction: column;
 `;
 const Title = styled.h2`
   text-align: center;
@@ -15,23 +18,40 @@ const Title = styled.h2`
   margin-bottom: 10px;
   font-size: 18px;
 `;
+const Area = styled.div<IAreaProps>`
+  background-color: ${(props) => (props.isDraggingOver ? "green" : props.isDraggingFromThis ? "red" : "blue")};
+  flex-grow: 1;
+  transition: background-color 0.3s ease-in-out;
+`;
 interface IBoardProps {
   toDos: string[];
   boardId: string;
 }
+interface IAreaProps{
+    isDraggingOver: boolean;
+    isDraggingFromThis: boolean;
+}
+
 function Board({ toDos, boardId }: IBoardProps) {
   return (
-    <Droppable droppableId={boardId}>
-      {(magic) => (
-        <BoardWrapper ref={magic.innerRef} {...magic.droppableProps}>
-          <Title>{boardId}</Title>
-          {toDos.map((toDo, index) => (
-            <DragabbleCard key={toDo} index={index} toDo={toDo} />
-          ))}
-          {magic.placeholder}
-        </BoardWrapper>
-      )}
-    </Droppable>
+    <BoardWrapper>
+      <Title>{boardId}</Title>
+      <Droppable droppableId={boardId}>
+        {(magic, snapshot) => (
+          <Area
+            isDraggingOver={snapshot.isDraggingOver}
+            isDraggingFromThis={Boolean(snapshot.draggingFromThisWith)}
+            ref={magic.innerRef}
+            {...magic.droppableProps}
+          >
+            {toDos.map((toDo, index) => (
+              <DragabbleCard key={toDo} index={index} toDo={toDo} />
+            ))}
+            {magic.placeholder}
+          </Area>
+        )}
+      </Droppable>
+    </BoardWrapper>
   );
 }
 
